f2a500b136ae3188d6372d4d19d04bc991d56460
Zachary Yu <zyu31415@gmail.com>
2026-02-19 16:03:21 -0800

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Arxiv Map",
   description: "Arxiv Map",
+  icons: {
+    icon: "/arxivmap.png",
+  },
 };
 
 export default function RootLayout({
